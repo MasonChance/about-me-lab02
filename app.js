d6c4f9113a1228b56dd7945e62a086b54fd74ce3
MasonChance <mason.fryberger@gmail.com>
2020-03-31 15:15:37 -0700

    outlined project with comments for ease of development

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// total of exatly 5 yes no questions
+// ensure validation of input accounting for Case sensitivity
+// console log for each question, comment out prior to submission
+
+
+// if/else statements for branched alerts
+// alerts to indicate correct answers
+
+
+
+// personalized greeting
+// personalized farewell
+
+
