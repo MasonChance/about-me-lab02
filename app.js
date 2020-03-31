c87ae0e37a7bc8fdaec8373b7247fc99fca887fe
MasonChance <mason.fryberger@gmail.com>
2020-03-31 16:02:20 -0700

    outlined JS App steps in comments with feature tasks

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 'use strict'
 
+
+
+
+// personalized greeting
+
+// var theirName = prompt("Hi, welcome to the project! What is your name?");
+//console.log(theirName : "");
+
+// var greeting = alert("We're glad to have you with us " + theirName + "! ");
+// console.log(greeting)
+
+// if else evaluating  response and triggering an alternate alert if no name is entered. "at this point in the project I am not allowed to use loops to end the prompt cycle, please enter your name"  
+// once correct answer is given, alert incorporates my bio. 
+
+
+
+
+
 // total of exatly 5 yes no questions
+
+// what was the name of the town I grew up in?    What was the first computer game I played?     How long have I been in the service/hospitality industry?     did my first business succeed? When did I decide to become a Software developer?     TRANSLATE ALL OF THESE INTO A YES OR NO
+
 // ensure validation of input accounting for Case sensitivity
 // console log for each question, comment out prior to submission
 
 
 // if/else statements for branched alerts
 // alerts to indicate correct answers
-
-
-
-// personalized greeting
+// alerts requesting specific answer format for false return values. 
 // personalized farewell
-
-
